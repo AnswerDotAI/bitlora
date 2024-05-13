9108784c3344ad50589cda151b6889e8dd3d7466
UmerHA <40663591+UmerHA@users.noreply.github.com>
2024-05-13 16:19:06 +0200

    triton/cuda in pytorch + hqq-qdora py reference
    - exploratory: use triton in pytorch fwd/bwd
    - exploratory: use cuda in pytorch fwd/bwd
    - full python implementation for hqq-qdora in python

---
+++ exploratory/gpu_in_pytorch_fwd_bwd/cuda/custom_linear.cpp
@@ -0,0 +1,18 @@
+#include <torch/extension.h>
+#include <vector>
+
+torch::Tensor forward(torch::Tensor weight, torch::Tensor x) {
+    return torch::mm(x, weight.t());
+}
+
+std::vector<torch::Tensor> backward(torch::Tensor d, torch::Tensor weight, torch::Tensor x) {
+    torch::Tensor dx = torch::mm(d, weight);
+    torch::Tensor dWeight = torch::mm(d.t(), x);
+    return {dWeight, dx};
+}
+
+// "custom_lin_cpp" is the name param in torch.utils.cpp_extension.load
+PYBIND11_MODULE(custom_lin_cpp, m) {
+    m.def("forward", &forward, "Custom linear forward");
+    m.def("backward", &backward, "Custom linear backward");
+}
---
+++ exploratory/gpu_in_pytorch_fwd_bwd/cuda/custom_linear_cuda.cpp
@@ -0,0 +1,34 @@
+#include <torch/extension.h>
+#include <vector>
+#include <iostream>
+
+// CUDA forward declarations
+std::vector<torch::Tensor> cuda_forward(torch::Tensor weight, torch::Tensor x);
+std::vector<torch::Tensor> cuda_backward(torch::Tensor d, torch::Tensor weight, torch::Tensor x);
+
+// C++ interface
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+std::vector<torch::Tensor> forward(torch::Tensor weight, torch::Tensor x) {
+  std::cout << "--- started forward" << std::endl;
+  CHECK_INPUT(weight);
+  CHECK_INPUT(x);
+  std::cout << "Shape of weight: " << weight.sizes() << std::endl;
+  std::cout << "Shape of x: " << x.sizes() << std::endl;
+  return cuda_forward(weight, x);
+}
+
+std::vector<torch::Tensor> backward(torch::Tensor d, torch::Tensor weight, torch::Tensor x) {
+  CHECK_INPUT(d);
+  CHECK_INPUT(weight);
+  CHECK_INPUT(x);
+  return cuda_backward(d, weight, x);
+}
+
+// "custom_lin_cpp" is the name param in torch.utils.cpp_extension.load
+PYBIND11_MODULE(custom_lin_cuda, m) {
+    m.def("forward", &forward, "Custom linear forward");
+    m.def("backward", &backward, "Custom linear backward");
+}
